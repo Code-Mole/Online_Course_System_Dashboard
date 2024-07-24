58b3b4ff4a6eba10a076452b8dd18433b9f9e8d7
Asangabia Mole <moleasangabia23@gmail.com>
2024-07-24 00:28:49 +0000

    setting the router system

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./Components/Dashboard.js";
+import Courses from "./Components/Courses.js";
+import Students from "./Components/Students.js";
+import Tutors from "./Components/Tutors.js";
+import Messages from "./Components/Messages.js";
+import SideBar from "./Components/SideBar.js";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <>
+      <div className="App">
+        <SideBar />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/Courses" element={<Courses />} />
+            <Route path="/Students" element={<Students />} />
+            <Route path="/Tutors" element={<Tutors />} />
+            <Route path="/Messages" element={<Messages />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
 }
 
 export default App;
