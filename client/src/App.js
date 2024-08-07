e5b476923cf8f2188b5fdae42bca4b3c202ce1c4
Asangabia Mole <moleasangabia23@gmail.com>
2024-08-07 23:38:03 +0000

    SideBar component

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,17 @@ import Students from "./Components/Students.js";
 import Tutors from "./Components/Tutors.js";
 import Messages from "./Components/Messages.js";
 import SideBar from "./Components/SideBar.js";
+import LogIn from "./Components/LogIn.js";
 
 function App() {
   return (
     <>
       <div className="App">
-        <SideBar />
         <Router>
+          <SideBar />
           <Routes>
-            <Route path="/" element={<Dashboard />} />
+            <Route path="/" element={<LogIn />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
             <Route path="/Courses" element={<Courses />} />
             <Route path="/Students" element={<Students />} />
             <Route path="/Tutors" element={<Tutors />} />
--- client/src/Components/SideBar.js
+++ client/src/Components/SideBar.js
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink, Link, useLocation } from "react-router-dom";
+import "./SideBar.css";
+// import MenuIcon from "@mui/icons-material/Menu";
 
 function SideBar() {
-  return <div>SideBar</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  let open = {
+    display: "block",
+  };
+  const location = useLocation();
+
+  return (
+    <div className="sideBar__container">
+      <nav className="navbar">
+        <div className="sideBar__logo">
+          <img className="logo" src="./images/logo.png" alt="logo.pic" />
+          <h1>
+            EduEmpower <span> Academy</span>
+          </h1>
+        </div>
+        <div
+          className="menuBtn"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
+          {/* <MenuIcon /> */}
+        </div>
+        <ul id="linksContainer" style={menuOpen ? open : {}}>
+          {location.pathname === "/login" || location.pathname === "/signup" ? (
+            <li>
+              <Link to="/">Go Back</Link>
+            </li>
+          ) : (
+            <>
+              <li>
+                <NavLink className="NavLink" to="/dashboard">
+                  Dashboard
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="NavLink" to="/students">
+                  Students
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="NavLink" to="/tutors">
+                  Tutors
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="NavLink" to="/messages">
+                  Messages <span id="Message__num">45</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="NavLink" to="/courses">
+                  Courses
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="NavLink" to="/addCourses">
+                  Add course
+                </NavLink>
+              </li>
+              <div className="LogOut">
+              <li>
+                <NavLink className="NavLink" to="/">
+                  LogOut
+                </NavLink>
+              </li>
+              </div>
+            </>
+          )}
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default SideBar;
